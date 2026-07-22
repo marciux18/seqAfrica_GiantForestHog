c3ad17f90c50118b5752fe7d6e1bed2537ec0bcc
Marta M. Ciucani <65894986+marciux18@users.noreply.github.com>
2026-07-22 11:37:33 +0200

    Update file paths in config_all.R

--- PSMC/plot/config_all.R
+++ PSMC/plot/config_all.R
@@ -2,7 +2,7 @@ mu <- 1.29e-8
 g <- 6
 i.iteration <- 25
 s <- 100
-files <-list.files("/psmc/PSMC_OUT/",".psmc",full.names=T)
+files <-list.files("/PSMC/PSMC_OUT/",".psmc",full.names=T)
 color_sp = c("#390099","#f77f00","#ffd166","#90a955", "#4f772d", "#ff006e")
 plot_name="All_hogs_psmc.png"
 
@@ -10,7 +10,7 @@ psmcFiles <- files
 
 inds <- gsub(".psmc", "",basename(psmcFiles))
 
-pop = as.data.frame(read.table("/psmc/PSMC/species_list.txt", 
+pop = as.data.frame(read.table("/PSMC/plot/species_list.txt", 
         header=F,
         col.names=c("Ind","Pop","Color"),
         sep="\t"))
